4040bf6fd095572e944ebb38784b00674c8fda8e
puzanshrestha <pujan.shrestha@sagarmatha.edu.np>
2017-02-14 20:00:23 +0545

    Added Color Quantity Dialog in Order

---
+++ app/src/main/java/com/example/pujan/bag/NDSpinner.java
@@ -0,0 +1,41 @@
+package com.example.pujan.bag;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Spinner;
+
+public class NDSpinner extends Spinner {
+
+    public NDSpinner(Context context)
+    { super(context); }
+
+    public NDSpinner(Context context, AttributeSet attrs)
+    { super(context, attrs); }
+
+    public NDSpinner(Context context, AttributeSet attrs, int defStyle)
+    { super(context, attrs, defStyle); }
+
+    @Override public void
+    setSelection(int position, boolean animate)
+    {
+        boolean sameSelected = position == getSelectedItemPosition();
+        super.setSelection(position, animate);
+        if (sameSelected) {
+            // Spinner does not call the OnItemSelectedListener if the same item is selected, so do it manually now
+            getOnItemSelectedListener().onItemSelected(this, getSelectedView(), position, getSelectedItemId());
+        }
+    }
+
+    @Override public void
+    setSelection(int position)
+    {
+        boolean sameSelected = position == getSelectedItemPosition();
+        super.setSelection(position);
+        if (sameSelected) {
+            // Spinner does not call the OnItemSelectedListener if the same item is selected, so do it manually now
+            getOnItemSelectedListener().onItemSelected(this, getSelectedView(), position, getSelectedItemId());
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pujan/bag/bagDetails/BagColorQuantity.java
@@ -0,0 +1,29 @@
+package com.example.pujan.bag.bagDetails;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by Pujan on 14-Feb-17.
+ */
+public class BagColorQuantity {
+    private int bag_id;
+    LinkedHashMap<String, Integer> quantityColor;
+
+
+    public int getBag_id() {
+        return bag_id;
+    }
+
+    public void setBag_id(int bag_id) {
+        this.bag_id = bag_id;
+    }
+
+    public LinkedHashMap<String, Integer> getQuantityColor() {
+        return quantityColor;
+    }
+
+    public void setQuantityColor(LinkedHashMap<String, Integer> quantityColor) {
+        this.quantityColor = quantityColor;
+    }
+}
